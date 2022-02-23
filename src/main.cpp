ac3dcb98aa8652326f8007bb869de33e3a69842f
mitch092 <stevenm7861@gmail.com>
2022-02-22 20:22:04 -0800

    Changed name of project to Raycer.

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@ import presenter;
 import vec2d;
 
 int main() {
-    Presenter presenter("RTIOW", Vec2d(500, 500));
+    Presenter presenter("Raycer", Vec2d(500, 500));
     while (presenter.is_running()) {
     }
 }
